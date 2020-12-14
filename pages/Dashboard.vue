a5c9e69f4657794471b96fa42491803a64d864f5
Ianosoro <43702447+lanosoro@users.noreply.github.com>
2020-12-13 23:04:53 -0800

    facebook_clone with vue

---
+++ pages/Dashboard.vue
@@ -0,0 +1,354 @@
+<template>
+  <v-app >
+    <v-app-bar
+      app
+      color="white"
+      flat
+      elevation="5"
+    >
+
+      <v-avatar style="position:absolute; margin-right:150px;"
+        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
+        size="32"
+      ></v-avatar>
+
+      
+       
+      
+      <v-avatar
+        class="hidden-sm-and-down"
+        size="32"
+      >
+      <v-img
+      
+      :width="width"
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/F_icon.svg/267px-F_icon.svg.png"
+    ></v-img>
+      </v-avatar>
+      <v-spacer></v-spacer>
+       <v-spacer></v-spacer>
+       <v-spacer></v-spacer>
+       <v-spacer></v-spacer>
+      <v-icon color="grey lighten-1" size="30">
+        mdi-home-outline
+      </v-icon>
+      <v-icon  color="grey lighten-1" size="30" class="ml-16">
+        mdi-flag-outline
+      </v-icon>
+      <v-icon color="grey lighten-1" size="30" class="ml-16">
+        mdi-cards-variant
+      </v-icon>
+      <v-icon color="grey lighten-1" size="30" class="ml-16">
+        mdi-television
+      </v-icon>
+       
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+     
+        <v-avatar
+        class="hidden-sm-and-down  ml-42"
+        color="grey lighten-1 shrink"
+        size="32"
+      >      
+          <v-img
+          :height="height"
+      :width="width"
+      src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+
+      </v-img>
+         </v-avatar>
+       
+          <p class="mt-4 pa-3" style="color:black;">Jon Dow</p>
+
+        
+        
+     
+       <v-avatar
+        class="hidden-sm-and-down float-right ml-4"
+        color="grey lighten-1 shrink"
+        size="32"
+      >
+      <v-icon color="black">
+        mdi-plus
+      </v-icon>
+      </v-avatar>
+       <v-avatar
+        class="hidden-sm-and-down float-right ml-4"
+        color="grey lighten-1 shrink"
+        size="32"
+      >
+      <v-icon color="black">
+        mdi-facebook-messenger
+      </v-icon>
+      </v-avatar>
+       <v-avatar
+        class="hidden-sm-and-down float-right ml-4"
+        color="grey lighten-1 shrink"
+        size="32"
+      >
+      <v-icon color="black">
+        mdi-bell
+      </v-icon>
+      
+      </v-avatar>
+       <v-avatar
+
+        class="hidden-sm-and-down pa-4 ml-4"
+        color="grey lighten-1 shrink"
+        size="32"
+      >
+      <v-icon color="black">
+        mdi-menu-down
+      </v-icon>
+      
+      </v-avatar>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="2"
+          >
+            
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="8"
+          >
+            <v-sheet
+              min-height="45vh"
+              rounded="sm"
+              color="white"
+            >
+             <v-layout column color="black"> 
+                <v-sheet
+              
+              min-height="37vh"
+              color="#fff"
+              flat
+            >
+              <v-img
+      :aspect-ratio="16/7"
+      :width="width"
+      src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+    ></v-img>
+     <v-layout row>
+        <v-avatar
+              class="mt-n15 ml-6"
+              size="188"
+              color="white"
+              style="border:5px solid #fff;"
+            >
+                    <v-img
+      
+      :width="width"
+      src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+    ></v-img>
+              
+            </v-avatar>
+           <v-flex> <h5  style="color:black" class="pl-12 pt-0 mt-8 ">John Dow</h5>
+           <v-icon class="pl-16 pt-n3 ml-16 mt-2" color="blue lighten-2">
+              mdi-check-circle
+            </v-icon></v-flex>
+            
+     </v-layout>
+            </v-sheet>
+            <v-tabs 
+       
+        background-color="white accent-4"
+        class="ml-n0 mt-4"
+        color="#3b5998 lighten-2"
+        style="color:blue;"
+       
+      >
+      <v-layout row>
+          <v-tab
+          style="color:blue;"
+           color="#3b5998 lighten-2"
+          class="ml-"
+          v-for="link in links"
+          :key="link"
+        >
+          {{ link }}
+        </v-tab>
+        
+      </v-layout>
+
+      </v-tabs>
+             </v-layout>
+            </v-sheet>
+          
+          <v-row>
+            <v-col cols="6"
+          sm="5">
+            <v-sheet
+              class="mt-n0"
+              min-height="400"
+              color="white"
+            >
+          <v-layout row>
+                <v-avatar
+        class="hidden-sm-and-down"
+        color="green lighten-1 shrink mt-3 ml-4"
+        size="32"
+      > <v-icon>
+        mdi-account
+        </v-icon></v-avatar>
+      <h4 style="color:black;" class="mt-4 ml-6">info</h4>
+          </v-layout>
+          <v-layout column>
+              <v-flex>
+              <v-icon color="grey lighten-1" class="mt-8 ml-2">
+                mdi-information
+              </v-icon>
+              <p style="color:black;" :width="width" class="ml-10 mt-n8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat eum eaque tempora sint</p>
+            </v-flex>
+             <v-flex>
+              <v-icon color="grey lighten-1" class="mt-0 ml-2">
+                mdi-thumb-up
+              </v-icon>
+              <p style="color:black;" :width="width" class="ml-10 mt-n6">120,000 people liked</p>
+            </v-flex>
+             <v-flex>
+              <v-icon color="grey lighten-1" class="mt-0 ml-2">
+                mdi-checkbox-multiple-marked
+              </v-icon>
+              <p style="color:black;" :width="width" class="ml-10 mt-n6">1,320,000 people follow</p>
+            </v-flex>
+             <v-flex>
+              <v-icon color="grey lighten-1" class="mt-0 ml-2">
+                mdi-map-marker
+              </v-icon>
+              <p style="color:black;" :width="width" class="ml-10 mt-n6">330,000 people checkdin here</p>
+            </v-flex>
+            <v-flex>
+              <v-icon color="grey lighten-1" class="mt-1 ml-2">
+                mdi-link
+              </v-icon>
+              <a href="" class="mt-n4" style="text-decoration:none;">https://jondow.com</a>
+            </v-flex>
+             <v-flex>
+              <v-icon color="grey lighten-1" class="mt-2 ml-2">
+                mdi-facebook-messenger
+              </v-icon>
+              <p style="color:black;" :width="width" class="ml-10 mt-n6"><a>send message</a></p>
+            </v-flex>
+
+          </v-layout>
+            </v-sheet>
+             <v-sheet
+              class="mt-3"
+              min-height="250"
+              color="white"
+            >
+          <v-layout row>
+                <v-avatar
+        class="hidden-sm-and-down"
+        color="pink darken-1 shrink mt-3 ml-4"
+        size="32"
+      >
+      <v-icon>
+        mdi-image
+      </v-icon>
+      </v-avatar>
+      <h4 class="mt-4 ml-6" style="color:black;">Photos</h4>
+          </v-layout>
+            </v-sheet>
+               <v-sheet
+              class="mt-3"
+              min-height="250"
+              color="white"
+            >
+              <v-avatar
+        class="hidden-sm-and-down"
+        color="deep-purple darken-1 shrink mt-3 ml-3"
+        size="32"
+      >
+      <v-icon>
+        mdi-earth
+        </v-icon>
+      </v-avatar>
+            </v-sheet>
+
+             </v-col>
+              <v-col cols="6"
+          sm="7">
+            <v-sheet
+            
+              min-height="150"
+              color="white"
+            >
+              
+            </v-sheet>
+              <v-sheet
+              class="mt-3"
+              min-height="550"
+              color="white"
+            >
+              
+            </v-sheet>
+          
+            <v-sheet
+              class="mt-3"
+              min-height="250"
+              color="white"
+            >
+            
+            </v-sheet>
+          
+          
+             </v-col>
+          </v-row>
+         <v-row>
+         
+          </v-row>
+           <v-row>
+             <v-col cols="6"
+          sm="5">
+         
+          </v-col>
+          
+             
+           </v-row>
+          </v-col>
+         
+          <v-col
+            cols="12"
+            sm="2"
+          >
+            
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'Home',
+        'Photos',
+        'About',
+        'Instagram',
+        'More'
+      ],
+    }),
+  }
+</script>
+<style  scoped>
+.profpic{
+ height: 80px;
+ width: 80px;
+ background: white;
+}
+img{
+  width: 100%;
+  height: auto;
+}
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,241 @@
+<template>
+ <v-container class="hidden-sm-and-down  " fluid fill-height ma-0 pa-0 style="height:850px; background-color:#f0f2f5; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;" >     
+    <v-row style="padding-bottom:112px position:absolute; background-color:#f0f2f5; min-height: 900px;
+   
+   clear: both;">
+      <v-col cols="12" md="4">
+        <div class="facb" style="position: absolute;
+    top: 20%;
+    left: 20%;
+    margin-top: -50px;
+    margin-left: -50px;">
+            <h1 style="color:#1877f2; font-size:54px;">facebook</h1>
+             <p style="display:inline; font-size:27px;"> Facebook helps you connect and share <br> with the people in your life.</p>
+        </div>
+
+         <v-card elevation="8" width="396px" height="390.35px"  class="mx-auto mt-10% mb" style="position: absolute;
+    top: 20%;
+    right: 20%;
+    margin-top: -50px;
+    margin-left: -50px;
+    border-radius:10px;" >
+      <v-card-text>
+        <v-form>
+          <v-text-field  single-line aria-required="red" style="color:#f0f2f5;"
+            outlined label="Email adress or Phone number" />
+          <v-text-field single-line
+            outlined label="Password" type="password"  />
+          <v-btn primary large block  style="background-color:#1877f2; color:#fff; text-transform: capitalize; font-size: 20px;
+line-height: 48px;" >
+           <strong>log in</strong> 
+          </v-btn>
+          <v-spacer ></v-spacer>
+          <v-spacer></v-spacer>
+          <span class="psw" style="text-align: center;
+    display: block;
+    margin: 0 auto;
+    padding:4%; text-decoration:none;"><a href="#">forgotten password?</a></span>
+    <v-divider></v-divider>
+   <v-flex column>
+     <popup/>
+   </v-flex>
+        </v-form>
+        <p  style=" 
+              min-width:360px;
+              display:inline;
+              color:#000;
+              margin: 0;
+              position: absolute;
+              top: 110%;
+              left: 59%;
+              transform: translate(-50%, -50%); 
+              
+              ">
+              <a style="color:black; font-family:SFProText-Semibold, Helvetica, Arial, sans-serif; cursor:pointer" color="black"><strong>Create a page</strong></a> for a celebrity, band or business.
+
+        </p>
+      </v-card-text>
+      
+    </v-card>
+    
+
+      </v-col>
+      </v-row>    
+     <v-row style="text-align:left; background:white; width:100%; height:212px; margin-bottom:0; position:absolute; top:900px;">  
+    <v-col id="footer_col">
+        <v-footer style="margin-bottom:0; position:absolute; background-color:white; width:900px; left:20%; @media (min-width: 900px){
+  
+    max-width: 100%;
+    position: relative;
+    margin-left: 0%;
+    float:left;
+    align-content: center;
+
+  } " class="justify-center">
+      
+        <v-card flat tile class="white lighten-1 #ccd0d5--text text-center pt-0" id="footer_content">
+          <v-card-text>
+            <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+       
+      <v-card-text class="#ccd0d5--text pt-0 " style="width:100%; ; " >
+        <ul id="horizontalist" class="justify-start ma-0" style="list-style-type:none; text-align:center; font-size:12px; ">
+        <li><a href="">English</a>  </li> <li><a href="">kiswahili</a> </li> <li><a href=""></a> </li> <li><a href="#">Español
+                      </a> </li> <li><a href="">Português (Brasil)
+                </a> </li><li><a href="">Italiano
+        </a>  </li> <li><a href="">Deutsch
+        </a> </li> <li><a href=""></a>中文(简体)
+              </li> <li><a href=""> Türkçe
+        </a> </li> <li><a href=""></a>हिन्दी
+
+              </li> <li><a href="">العربية
+        </a> </li> <li><a href=""></a>中文(简体)
+              </li>
+         <a href=""><v-icon depressed
+                    elevation="2"
+                   
+                    outlined
+                   small
+                    tile style="background-color:#f0f2f5; border-style: solid;  border-width: 1px; border-radius:3px; padding:0px 10px;  ">
+                   mdi-plus 
+                               </v-icon></a>
+         
+         
+          
+         
+        </ul>
+      </v-card-text>
+      <v-divider></v-divider>
+
+       <p style="text-align:center;">  <ul class="b">
+           <li><a href="">Signup</a></li>
+           <li><a href="">Login</a></li>
+           <li><a href="">Messenger</a></li>
+           <li><a href="">Facebooklight</a></li>
+           <li><a href="">watch</a></li>
+           <li><a href="">people</a></li>
+           <li><a href="">pages</a></li>
+           <li><a href="">page category</a></li>
+           <li><a href="">places</a></li>
+           <li><a href="">games</a></li>
+           <li><a href="">locations</a></li>
+           <li><a href="">facebookPay</a></li>
+           <li><a href="">marketplace</a></li>
+           <li><a href="">groups</a></li>
+           <li><a href="">Signup</a></li>
+           <li><a href="">Login</a></li><br>
+           <li><a href="">Messenger</a></li>
+           <li><a href="">Facebooklight</a></li>
+           <li><a href="">watch</a></li>
+           <li><a href="">people</a></li>
+           <li><a href="">pages</a></li>
+           <li><a href="">page category</a></li>
+           <li><a href="">places</a></li>
+           <li><a href="">games</a></li>
+           <li><a href="">locations</a></li>
+           <li><a href="">facebookPay</a></li>
+           <li><a href="">marketplace</a></li>
+           <li><a href="">groups</a></li><br>
+              <v-card-text class="#737373--text pl-0" style="padding: 30px 0px; font-size:12px; left:0; color:transparent;" >
+       Facebook © {{ new Date().getFullYear() }}
+      </v-card-text>
+
+           </ul>   </p>
+  
+      
+      <div class="text-xs-left" >
+         <v-card-text class="#737373--text pl-0" style="margin-bottom:30px; font-size:12px; left:0; color:#737373;" >
+       Facebook © {{ new Date().getFullYear() }}
+      </v-card-text>
+      </div>
+     
+    </v-card>
+  
+  
+  
+  </v-footer>
+       </v-col>
+     </v-row>
+     <div id="example">{{ message }}</div>
+   
+
+  </v-container>
+
+</template>
+
+<script>
+import popup from '../components/popup.vue'
+
+export default {
+  components: { popup },
+    name: 'loginweb',
+
+    data: () => ({
+      dialogue:false
+     
+    
+    }),
+  }
+
+</script>
+<script>
+export default {
+ 
+}
+</script>
+
+<style  scoped>
+ 
+
+li{
+  float: left;
+  line-height: 30px;
+  padding: 0px 10px;
+  word-wrap: normal;
+  list-style-type:none;
+
+}
+li a{
+ 
+  color: #737373;
+  text-decoration: none; /* no underline */
+  margin-right: 0%;
+ 
+}
+ul{
+  list-style-type:none;
+  font-size: 12px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; 
+}
+ul.b{
+  line-height: 200%;
+  text-align: justify;
+}
+a{
+  text-decoration: none;
+}
+a:hover{
+  text-decoration: underline;
+}
+@media (min-width: 900px){
+  .footer_content{
+    max-width: 100%;
+    position: relative;
+    margin-left: 0%;
+    float:left;
+    align-content: center;
+
+  }
+  v-row{
+    display: none;
+  }
+  .footer_col{
+    display: none;
+  }
+}
+
+</style>
